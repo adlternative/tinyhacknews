e5316f1027c8d646eb13bd05139acd20c027d4a4
ZheNing Hu <adlternative@gmail.com>
2024-01-28 15:40:48 +0800

    feat(all): init spring-boot project
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

---
+++ src/main/java/com/adlternative/tinyhacknews/TinyHacknewsApplication.java
@@ -0,0 +1,12 @@
+package com.adlternative.tinyhacknews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyHacknewsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TinyHacknewsApplication.class, args);
+  }
+}
---
+++ src/test/java/com/adlternative/tinyhacknews/TinyHacknewsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.adlternative.tinyhacknews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyHacknewsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
