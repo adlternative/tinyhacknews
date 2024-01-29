1cba7f548638b1a5d5e81c30ee4938c8cb14ef89
ZheNing Hu <adlternative@gamil.com>
2024-01-29 18:57:27 +0800

    chore(user): add schema.sql which record db tables
    Signed-off-by: ZheNing Hu <adlternative@gamil.com>

---
+++ src/main/java/com/adlternative/tinyhacknews/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users (
+                      id INT PRIMARY KEY AUTO_INCREMENT,
+                      username VARCHAR(50) NOT NULL,
+                      password VARCHAR(255) NOT NULL,
+                      email VARCHAR(50) NOT NULL,
+                      createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                      updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
