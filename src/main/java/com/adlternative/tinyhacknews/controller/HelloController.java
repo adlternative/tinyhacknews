d50539e76a5c32da6725e1b045a8083c89654223
ZheNing Hu <adlternative@gamil.com>
2024-04-22 17:25:29 +0800

    feat(hello): add POST method for testing

--- src/main/java/com/adlternative/tinyhacknews/controller/HelloController.java
+++ src/main/java/com/adlternative/tinyhacknews/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.adlternative.tinyhacknews.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ public class HelloController {
   @GetMapping
   public String hello() {
     return "Hello, Spring Boot!";
+  }
+
+  @PostMapping
+  public String echo(@RequestBody String body) {
+    return body;
   }
 }
